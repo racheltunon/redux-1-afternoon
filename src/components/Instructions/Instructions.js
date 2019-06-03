0475118506e9b0a82f2a57e67e2ca694eb73eb9d
Rachel Tuñon <racheltunon@Rachels-iMac.local>
2019-06-03 16:44:45 -0500

    done

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { ADD_INSTRUCTION, ADD_RECIPE } from "../../store";
+
 
 class Instructions extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      instructions: [],
+    const reduxState= store.getState();
+     this.state = {
+      instructions: reduxState.instructions,
       input: ""
     };
   }
@@ -15,13 +18,26 @@ class Instructions extends Component {
     });
   }
   addInstruction() {
-    // Send data to Redux state
+    store.dispatch({
+      type: ADD_INSTRUCTION,
+      payload: this.state.input
+    });
     this.setState({
       input: ""
     });
   }
   create() {
-    // Create new recipe in Redux state
+    store.dispatch({
+      type: ADD_RECIPE
+    })
+  }
+  componentDidMount(){
+    store.subscribe(() => {
+      const reduxState = store.getState();
+      this.setState({
+        instructions: reduxState.instructions
+      })
+    })
   }
   render() {
     const instructions = this.state.instructions.map((instruction, i) => {
